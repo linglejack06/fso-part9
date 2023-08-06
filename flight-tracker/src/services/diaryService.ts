19fc0935754052e3ee6e57a70699694d141799e9
Jack Lingle <captainjack0818@gmail.com>
2023-08-06 10:01:44 -0400

    add typed values and data

---
+++ flight-tracker/src/services/diaryService.ts
@@ -0,0 +1,14 @@
+import diaries from '../../data/entries';
+import { DiaryEntry } from '../types';
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+};
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary,
+};
