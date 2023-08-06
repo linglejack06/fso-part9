2f4df6fd277c58b7d1eecfb31716570eddf17f67
Jack Lingle <captainjack0818@gmail.com>
2023-08-06 09:52:52 -0400

    add data plus diary router

--- flight-tracker/src/index.ts
+++ flight-tracker/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diaryRouter from './routes/diaries';
 
 const app = express();
 app.use(express.json());
@@ -6,6 +7,7 @@ app.use(express.json());
 app.get('/ping', (_req, res) => {
   res.send('ping');
 });
+app.use('/api/diaries', diaryRouter);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
