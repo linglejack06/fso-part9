0e39b92ccf6367f9d97aec2b9b83dfc9573cbae8
Jack Lingle <captainjack0818@gmail.com>
2023-08-10 13:16:13 -0400

    add cors

--- flight-tracker/src/index.ts
+++ flight-tracker/src/index.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import express from 'express';
 import diaryRouter from './routes/diaries';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/ping', (_req, res) => {
   res.send('ping');
