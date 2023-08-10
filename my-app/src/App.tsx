3613c621af4be60caa755be047bfec4cddf68104
Jack Lingle <captainjack0818@gmail.com>
2023-08-10 11:47:00 -0400

    add deeper type usage to support different object variants

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,35 @@
 import Contents from './components/Contents';
 import Total from './components/Total';
 import Welcome from './components/welcome'
+import { CoursePart } from './types';
 
 function App() {
   const courseName = 'Half Stack Application Development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basic of type narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
     },
   ]
   const total = courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)
